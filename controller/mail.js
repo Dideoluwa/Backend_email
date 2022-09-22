9f1f87e59d079b5aed15ced503984772c1dbdf3b
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2022-09-22 19:08:08 +0100

    updated Backend

--- controller/mail.js
+++ controller/mail.js
@@ -5,6 +5,8 @@ exports.sendMail = (req, res) => {
     console.log(req.body)
     let userMail = req.body.email
     let name = req.body.name
+    let dates = req.body.dates
+    let timer = req.body.timer
 
     let transporter = nodeMailer.createTransport({
         service: "Gmail",
@@ -17,20 +19,20 @@ exports.sendMail = (req, res) => {
     let message = {
         from: process.env.EMAIL,
         to: userMail,
-        subject: "this is test",
-        text: name,
+        subject: `We just scheduled a demo for you, ${name}.`,
+        html: `<b>Hi ${name}, <br>A new event has been scheduled.<br>Event date/Time: <br>${dates}/${timer}<br>Invitee:<br><a>${userMail}<a/>.</b >`
     }
 
     transporter.sendMail(message, (err, info) => {
         if (err) {
             console.log("error in sending mail", err)
             return res.status(400).json({
-                message : `error in sending mail ${err}`
+                message: `error in sending mail ${err}`
             })
-        } else{
+        } else {
             console.log(`success in sending message ${info}`)
             return res.json({
-                message : info
+                message: info
             })
         }
     })
