8ff4225218a8e56fcc3dec137c7c50a572162faa
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2022-09-22 05:26:18 +0100

    Backend

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+const Mail = require('./routes/mail')
+
+app.use("/api", Mail)
+
+app.listen(8000, () => {
+    console.log(`Server Running at 8000`);
+})
---
+++ controller/mail.js
@@ -0,0 +1,37 @@
+const nodeMailer = require("nodemailer");
+const dotenv = require('dotenv');
+dotenv.config()
+exports.sendMail = (req, res) => {
+    console.log(req.body)
+    let userMail = req.body.email
+    let name = req.body.name
+
+    let transporter = nodeMailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    })
+
+    let message = {
+        from: process.env.EMAIL,
+        to: userMail,
+        subject: "this is test",
+        text: name,
+    }
+
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log("error in sending mail", err)
+            return res.status(400).json({
+                message : `error in sending mail ${err}`
+            })
+        } else{
+            console.log(`success in sending message ${info}`)
+            return res.json({
+                message : info
+            })
+        }
+    })
+}
